30b4f068f8673be1ad271bea9d26e6fa609fb3fa
@javierlinked <javierlinked@gmail.com>
2021-09-23 17:16:11 +0200

    add logging

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -1,12 +1,18 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.5.0;
+pragma solidity ^0.8.3;
 
 contract Adoption {
     address[16] public adopters;
+    
+    event LogAdopter(address indexed _adopter, uint _pet);
 
-// Adopting a pet
+    // Adopting a pet
     function adopt(uint petId) public returns (uint) {
         require(petId >= 0 && petId <= 15);
+        // if pet already adopted throw
+        require(adopters[petId] == address(0));
+
+        emit LogAdopter(adopters[petId], petId);
 
         adopters[petId] = msg.sender;
 
